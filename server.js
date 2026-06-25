3ddba2f3f48f579a1c73282def1b5d2f83c70e37
cfutrell82-alt <cfutrell82@icloud.com>
2026-06-25 06:07:52 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
+process.env.AUTH_PORT = '3000';
+process.env.STRIPE_PORT = '3001';
+process.env.SIGNALS_PORT = '3002';
+
 const { spawn } = require('child_process');
 
 const services = [
-  'alphaedge-auth.js',
-  'alphaedge-stripe-server.js',
-  'alphaedge-signals.js'
+  { file: 'alphaedge-auth.js', port: '3000' },
+  { file: 'alphaedge-stripe-server.js', port: '3001' },
+  { file: 'alphaedge-signals.js', port: '3002' },
 ];
 
-services.forEach(service => {
-  const proc = spawn('node', [service], { stdio: 'inherit' });
-  proc.on('close', code => console.log(`${service} exited with code ${code}`));
-  console.log(`Started ${service}`);
+services.forEach(({ file, port }) => {
+  const env = { ...process.env, PORT: port };
+  const proc = spawn('node', [file], { stdio: 'inherit', env });
+  proc.on('close', code => console.log(`${file} exited with code ${code}`));
+  console.log(`Started ${file} on port ${port}`);
 });
